4dddfde518139315cea8146548eb049437bc5d6d
masterful-messaging-7603 <alicebabatunde15@gmail.com>
2018-09-30 18:08:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (katz_deli)
   if katz_deli.empty?  
     puts "The line is currently empty." 
   else
-    $stdout.sync = true
+    
     print "The line is currently: "
     katz_deli.each_with_index{|name, index| 
     print "#{index + 1}. #{name}"
