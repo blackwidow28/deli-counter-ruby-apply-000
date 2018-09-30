7d53ffb37d054a163ab3b30039086309c40c3de1
masterful-messaging-7603 <alicebabatunde15@gmail.com>
2018-09-30 18:16:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line (katz_deli)
   if katz_deli.empty?  
     puts "The line is currently empty." 
   else
-    current = "The line is currently: "
+    current = "The line is currently:"
     katz_deli.each_with_index do |name, index| 
-    current += " #{index + 1}. #{name}"
+    current += "#{index + 1}. #{name}"
    end
 
     puts "#{current}" 
